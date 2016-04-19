2be6264311a5e784a1ff095c102c1bff6bbce3ae
zachnewburgh <zach.newburgh@gmail.com>
2016-04-19 07:49:32 -0400

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -19,7 +19,10 @@ function ContactCard() {
 				'<span class="username">{{ username }}</span>',
 			'</div>'
 		].join(''),
-		restrict: 'E'
+		restrict: 'E',
+		controller: function($scope) {
+			$scope.username = $scope.username.toLowerCase();
+		}
 	};
 }
 
